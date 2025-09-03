c8ce2b0dee3227143eb25cd56d4bb71f52766eab
Guayavas <victortdh1@gmail.com>
2025-09-03 16:54:00 -0500

    Parte 1 echa

--- Proyect-Angular-19/src/app/app.component.ts
+++ Proyect-Angular-19/src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Ejemplo1Component } from './ejemplo1/ejemplo1.component';
-import { micomponente } from './personas/personas.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Ejemplo1Component,micomponente],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
