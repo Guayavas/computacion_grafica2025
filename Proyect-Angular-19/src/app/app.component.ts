3fd97bed1e74fc2d02740c78a212112c2b25b4e7
Guayavas <victortdh1@gmail.com>
2025-09-03 16:00:33 -0500

    Ya funciona el componente

--- Proyect-Angular-19/src/app/app.component.ts
+++ Proyect-Angular-19/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Ejemplo1Component } from './ejemplo1/ejemplo1.component';
+import { micomponente } from './personas/personas.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Ejemplo1Component,micomponente],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Proyect-Angular-19/src/app/personas/personas.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "personas-app",
+    templateUrl: "./personas.component.html"
+}
+)
+
+export class micomponente{
+    public mensaje = "mensaje preciososo"
+    public segundomensaje = "jajaja"
+
+}
