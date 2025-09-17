1190e46a7e28001fa9601fc97200823ed7578200
Guayavas <victortdh1@gmail.com>
2025-09-17 17:15:43 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+//import { Button } from "primeng/button";
+import { ButtonComponent } from "./button/button.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet/*, Button*/, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
